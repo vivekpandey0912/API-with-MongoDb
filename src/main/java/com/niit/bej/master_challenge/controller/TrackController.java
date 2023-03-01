6f5d86739050dd4364452a837ca7ca47f03e2bf2
Vivek pandey <vp1446392@gmail.com>
2023-03-01 22:53:47 +0530

    added method for addTrack

--- src/main/java/com/niit/bej/master_challenge/controller/TrackController.java
+++ src/main/java/com/niit/bej/master_challenge/controller/TrackController.java
@@ -1,7 +1,17 @@
 package com.niit.bej.master_challenge.controller;
 
+import com.niit.bej.master_challenge.domain.Track;
+import com.niit.bej.master_challenge.exception.TrackAlreadyExist;
+import com.niit.bej.master_challenge.exception.TrackNotFound;
 import com.niit.bej.master_challenge.service.TrackService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TrackController {
@@ -10,6 +20,30 @@ public class TrackController {
 
     public TrackController(TrackService trackService) {
         this.trackService = trackService;
+    }
+
+    @PostMapping("/track/add")
+    public ResponseEntity<?> addTrack(@RequestBody Track track) throws TrackAlreadyExist {
+        try {
+            Track track1 = trackService.addTrack(track);
+            return new ResponseEntity<>(track1, HttpStatus.ACCEPTED);
+        } catch (TrackAlreadyExist trackAlreadyExist) {
+            throw new TrackAlreadyExist();
+        } catch (Exception exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
+    @GetMapping("/track/alltrack")
+    public ResponseEntity<?> getAllTracks() throws TrackNotFound {
+        try {
+            List<Track> trackList = trackService.getAllTrack();
+            return new ResponseEntity<>(trackList, HttpStatus.FOUND);
+        } catch (TrackNotFound trackNotFound) {
+            throw new TrackNotFound();
+        } catch (Exception exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+        }
     }
 
 
