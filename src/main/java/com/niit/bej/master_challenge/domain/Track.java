34de78be44182e7089ee17caeaff3fac0c9465c1
Vivek pandey <vp1446392@gmail.com>
2023-03-01 17:39:33 +0530

    fields added in Track.java

--- src/main/java/com/niit/bej/master_challenge/domain/Track.java
+++ src/main/java/com/niit/bej/master_challenge/domain/Track.java
@@ -1,5 +1,15 @@
 package com.niit.bej.master_challenge.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 public class Track {
+
+    @Id
+    private int id;
+    private String name;
+    private int rating;
+    private Artist artist;
+
 }
