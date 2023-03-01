6158549488e5c2e66caeeca854951fde524eeb9e
Vivek pandey <vp1446392@gmail.com>
2023-03-01 17:34:12 +0530

    hashcode and equals method added

--- src/main/java/com/niit/bej/master_challenge/domain/Artist.java
+++ src/main/java/com/niit/bej/master_challenge/domain/Artist.java
@@ -1,5 +1,7 @@
 package com.niit.bej.master_challenge.domain;
 
+import java.util.Objects;
+
 public class Artist {
 
     private int artistId;
@@ -27,5 +29,18 @@ public class Artist {
 
     public void setArtistName(String artistName) {
         this.artistName = artistName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artist artist = (Artist) o;
+        return artistId == artist.artistId && Objects.equals(artistName, artist.artistName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(artistId, artistName);
     }
 }
