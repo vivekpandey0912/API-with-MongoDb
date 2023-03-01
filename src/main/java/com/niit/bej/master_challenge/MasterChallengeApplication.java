526900b0de78d41abc9edfa9f93b12fe64f689de
Vivek pandey <vp1446392@gmail.com>
2023-03-01 17:26:18 +0530

    initial commit

---
+++ src/main/java/com/niit/bej/master_challenge/MasterChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.niit.bej.master_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niit/bej/master_challenge/MasterChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.bej.master_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
