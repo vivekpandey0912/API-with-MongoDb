7235d41c6602251c68c73755d6f286d43c56863c
Vivek pandey <vp1446392@gmail.com>
2023-03-01 22:22:43 +0530

    constructor added in TrackServiceImpl.java

--- src/main/java/com/niit/bej/master_challenge/service/TrackServiceImpl.java
+++ src/main/java/com/niit/bej/master_challenge/service/TrackServiceImpl.java
@@ -1,4 +1,33 @@
 package com.niit.bej.master_challenge.service;
 
-public class TrackServiceImpl {
+import com.niit.bej.master_challenge.domain.Track;
+import com.niit.bej.master_challenge.exception.TrackAlreadyExist;
+import com.niit.bej.master_challenge.exception.TrackNotFound;
+import com.niit.bej.master_challenge.repository.TrackRepository;
+
+import java.util.List;
+
+public class TrackServiceImpl implements TrackService {
+
+    private final TrackRepository trackRepository;
+
+    public TrackServiceImpl(TrackRepository trackRepository) {
+        this.trackRepository = trackRepository;
+    }
+
+
+    @Override
+    public Track addTrack(Track track) throws TrackAlreadyExist {
+        return null;
+    }
+
+    @Override
+    public List<Track> getAllTrack() throws TrackNotFound {
+        return null;
+    }
+
+    @Override
+    public boolean deleteTrackById(Integer id) throws TrackNotFound {
+        return false;
+    }
 }
