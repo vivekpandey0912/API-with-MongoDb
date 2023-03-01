6bf133d220bc1363f61a1f814937cea0a19f3a9c
Vivek pandey <vp1446392@gmail.com>
2023-03-01 21:55:27 +0530

    repository package created and added TrackRepository.java and extended MongoRepository

---
+++ src/main/java/com/niit/bej/master_challenge/repository/TrackRepository.java
@@ -0,0 +1,9 @@
+package com.niit.bej.master_challenge.repository;
+
+import com.niit.bej.master_challenge.domain.Track;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrackRepository extends MongoRepository<Track, Integer> {
+}
