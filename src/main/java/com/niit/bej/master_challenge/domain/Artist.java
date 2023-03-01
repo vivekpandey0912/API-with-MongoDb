89273121180562b7a8c06161f7a1a34b3308e745
Vivek pandey <vp1446392@gmail.com>
2023-03-01 17:33:44 +0530

    getter and setters method added

--- src/main/java/com/niit/bej/master_challenge/domain/Artist.java
+++ src/main/java/com/niit/bej/master_challenge/domain/Artist.java
@@ -12,4 +12,20 @@ public class Artist {
 
     public Artist() {
     }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
 }
