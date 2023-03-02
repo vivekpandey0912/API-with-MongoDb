18e3788762a6a0090bac7aa8cbae2a4b09be7592
Vivek pandey <vp1446392@gmail.com>
2023-03-02 19:12:05 +0530

    created TrackServiceImplTest.java

---
+++ src/test/java/com/niit/bej/master_challenge/service/TrackServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.niit.bej.master_challenge.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrackServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addTrack() {
+    }
+
+    @Test
+    void getAllTrack() {
+    }
+
+    @Test
+    void deleteTrackById() {
+    }
+
+    @Test
+    void trackSearchByArtistName() {
+    }
+
+    @Test
+    void trackRatingGreaterThanFour() {
+    }
+}
