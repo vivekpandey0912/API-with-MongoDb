895d17daf45c5e8eb7b3b1e50a6dbdf807193abb
Vivek pandey <vp1446392@gmail.com>
2023-03-02 19:42:28 +0530

    added TestCase for addTrackSuccess

--- src/test/java/com/niit/bej/master_challenge/service/TrackServiceImplTest.java
+++ src/test/java/com/niit/bej/master_challenge/service/TrackServiceImplTest.java
@@ -1,29 +1,68 @@
 package com.niit.bej.master_challenge.service;
 
+import com.niit.bej.master_challenge.domain.Artist;
+import com.niit.bej.master_challenge.domain.Track;
+import com.niit.bej.master_challenge.exception.TrackAlreadyExist;
+import com.niit.bej.master_challenge.exception.TrackNotFound;
+import com.niit.bej.master_challenge.repository.TrackRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+//import static org.springframework.data.mongodb.core.aggregation.ConditionalOperators.Switch.CaseOperator.when;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class TrackServiceImplTest {
+
+
+    @Mock
+    TrackRepository trackRepository;
+
+    @InjectMocks
+    TrackServiceImpl trackServiceImpl;
+
+
+    Track track;
+    Artist artist;
+
 
     @BeforeEach
     void setUp() {
 
-
+        artist = new Artist(1,"Arijit");
+        track = new Track(2,"Rom",4,artist);
     }
 
     @AfterEach
     void tearDown() {
+        track = null;
+        artist = null;
     }
 
     @Test
-    void addTrack() {
+    void addTrackSuccess() throws TrackAlreadyExist {
+
+        when(trackRepository.findById(any())).thenReturn(Optional.ofNullable(null));
+        when(trackRepository.save(any())).thenReturn(track);
+        assertEquals(track,trackServiceImpl.addTrack(track));
+        verify(trackRepository,times(1)).findById(any());
+        verify(trackRepository,times(1)).save(any());
+
+
     }
 
     @Test
-    void getAllTrack() {
+    void getAllTrack() throws TrackNotFound
+    {
+
     }
 
     @Test
